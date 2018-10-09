7fff388d1d8cba0a122ba0db5091823d8ebe368a
George Okojie Davis <binarygeotech@users.noreply.github.com>
2018-10-09 23:32:00 +0100

    Updated Layout to switch to Full Layout
    Added condition to either use full layout or regular layout.
    This option was added to enable AdminLTE Login page properly.

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @if (config('backpack.base.meta_robots_content'))
-    <meta name="robots" content="{{ config('backpack.base.meta_robots_content', 'noindex, nofollow') }}">
-    @endif
 
     {{-- Encrypted CSRF token for Laravel, in order for Ajax requests to work --}}
     <meta name="csrf-token" content="{{ csrf_token() }}" />
@@ -21,8 +18,8 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/backpack/') }}/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('vendor/backpack/') }}/ionicons/ionicons.min.css">
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
@@ -31,20 +28,21 @@
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/plugins/pace/pace.min.css">
     <link rel="stylesheet" href="{{ asset('vendor/backpack/pnotify/pnotify.custom.min.css') }}">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> --}}
+    <link rel="stylesheet" href="{{ asset('fonts/gfont.css') }}">
 
     <!-- BackPack Base CSS -->
     <link rel="stylesheet" href="{{ asset('vendor/backpack/backpack.base.css') }}?v=2">
-    @if (config('backpack.base.overlays') && count(config('backpack.base.overlays')))
-        @foreach (config('backpack.base.overlays') as $overlay)
-        <link rel="stylesheet" href="{{ asset($overlay) }}">
-        @endforeach
-    @endif
-
+    <link rel="stylesheet" href="{{ asset('vendor/backpack/overlays/backpack.bold.css') }}">
 
     @yield('after_styles')
     @stack('after_styles')
 
+    <style type="text/css">
+        .text-left {
+            text-align: left !important;
+        }
+    </style>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -52,8 +50,10 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body class="hold-transition {{ config('backpack.base.skin') }} sidebar-mini">
-	<script type="text/javascript">
+<body class="hold-transition {{ config('backpack.base.skin') }} {{ (!isset($fullLayout) || (isset($fullLayout) && !$fullLayout))?'sidebar-mini':'' }} {{ isset($bodyClass)?$bodyClass:'' }}">
+
+@if(!isset($fullLayout) || (isset($fullLayout) && !$fullLayout))
+    <script type="text/javascript">
 		/* Recover sidebar state */
 		(function () {
 			if (Boolean(sessionStorage.getItem('sidebar-toggle-collapsed'))) {
@@ -109,16 +109,18 @@
       <!-- /.content-wrapper -->
 
       <footer class="main-footer">
-        @if (config('backpack.base.show_powered_by'))
+        {{-- @if (config('backpack.base.show_powered_by'))
             <div class="pull-right hidden-xs">
               {{ trans('backpack::base.powered_by') }} <a target="_blank" href="http://backpackforlaravel.com?ref=panel_footer_link">Backpack for Laravel</a>
             </div>
-        @endif
+        @endif --}}
         {{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
       </footer>
     </div>
     <!-- ./wrapper -->
-
+@elseif(isset($fullLayout) && $fullLayout)
+    @yield('content')
+@endif
 
     @yield('before_scripts')
     @stack('before_scripts')
@@ -136,15 +138,6 @@
 
     <!-- page script -->
     <script type="text/javascript">
-        /* Store sidebar state */
-        $('.sidebar-toggle').click(function(event) {
-          event.preventDefault();
-          if (Boolean(sessionStorage.getItem('sidebar-toggle-collapsed'))) {
-            sessionStorage.setItem('sidebar-toggle-collapsed', '');
-          } else {
-            sessionStorage.setItem('sidebar-toggle-collapsed', '1');
-          }
-        });
         // To make Pace works on Ajax calls
         $(document).ajaxStart(function() { Pace.restart(); });
 
@@ -154,6 +147,16 @@
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
+    @if(!isset($fullLayout) || (isset($fullLayout) && !$fullLayout))
+        /* Store sidebar state */
+        $('.sidebar-toggle').click(function(event) {
+          event.preventDefault();
+          if (Boolean(sessionStorage.getItem('sidebar-toggle-collapsed'))) {
+            sessionStorage.setItem('sidebar-toggle-collapsed', '');
+          } else {
+            sessionStorage.setItem('sidebar-toggle-collapsed', '1');
+          }
+        });
 
         // Set active state on menu element
         var current_url = "{{ Request::fullUrl() }}";
@@ -177,6 +180,8 @@
         $('.nav-tabs a').on('shown.bs.tab', function (e) {
             location.hash = e.target.hash.replace("#tab_", "#");
         });
+    @endif
+        @yield('custom_scripts')
     </script>
 
     @include('backpack::inc.alerts')
